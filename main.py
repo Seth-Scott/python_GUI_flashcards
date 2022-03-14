a2442558f9bb5a1b8dd40a30addaac7bdbd142b2
Seth Scott <s.scottvancouverwa@gmail.com>
2022-03-13 22:32:40 -0700

    add basic flashcard functionality

--- main.py
+++ main.py
@@ -1,14 +1,54 @@
 from tkinter import *
+import pandas, random, time
 
+data = pandas.read_csv("data/french_words.csv")
+to_learn = data.to_dict(orient="records")
+current_card = {}
+
+print(to_learn)
 BACKGROUND_COLOR = "#B1DDC6"
+
+
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(canvas_image, image=card_front)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(canvas_image, image=card_back)
+
+
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+flip_timer = window.after(3000, func=flip_card)
+
 
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front)
-canvas.grid(row=0, column=0)
+card_back = PhotoImage(file="images/card_back.png")
+canvas_image = canvas.create_image(400, 263, image=card_front)
+canvas.itemconfig(canvas_image, image=card_front)
+card_title = canvas.create_text(400, 150, text="", fill="black", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", fill="black", font=("Ariel", 60, "bold"))
+canvas.grid(row=0, column=0, columnspan=2)
 
+
+wrong = PhotoImage(file="images/wrong.png")
+incorrect_button = Button(image=wrong, highlightthickness=0, command=next_card)
+incorrect_button.grid(row=1, column=0)
+
+right = PhotoImage(file="images/right.png")
+correct_button = Button(image=right, highlightthickness=0, command=next_card)
+correct_button.grid(row=1, column=1)
+
+next_card()
 
 window.mainloop()
